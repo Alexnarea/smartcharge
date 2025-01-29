d21b0365a76a0ec5253fa035ad2c773d86c683f0
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2025-01-28 19:50:55 -0500

    Base de Datos

--- src/main/resources/db.migration/V1__initial.sql
+++ src/main/resources/db.migration/V1__initial.sql
@@ -1,9 +1,19 @@
-CREATE TABLE IF NOT EXISTS user
-(
+CREATE TABLE IF NOT EXISTS user (
     id        INT PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
     age       INT NOT NULL,
     telephone VARCHAR(100),
     email     VARCHAR(255) NOT NULL,
-
+    )
+CREATE TABLE IF NOT EXISTS events (
+    id       INT PRIMARY KEY,
+    battery  INT NOT NULL,
+    km       DECIMAL(10,2) NOT NULL,
+    time     TIME NOT NULL
     );
+CREATE TABLE IF NOT EXISTS roles (
+    id   INT PRIMARY KEY,
+    fullnameAdmin VARCHAR(50) NOT NULL
+    User_id INT
+    );
+
